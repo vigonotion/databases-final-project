53a880d4fd7308ef525b061ae603cdc625c621c9
pokealai <poxx0003@stud.kea.dk>
2019-11-29 21:39:05 +0100

    updated schema to reflect erd

--- database/000_Schema.sql
+++ database/000_Schema.sql
@@ -5,73 +5,74 @@ GO
 
 
 CREATE TABLE TCity(
-	zip_code CHAR(4) NOT NULL,
-	city VARCHAR(100) NOT NULL,
-	PRIMARY KEY(zip_code)
+	cZip_code CHAR(4) NOT NULL,
+	cCity VARCHAR(100) NOT NULL,
+	PRIMARY KEY(cZip_code)
 )
 
 CREATE TABLE TUser(
-	email VARCHAR(100) NOT NULL,
-	first_name VARCHAR(100) NOT NULL,
-	last_name VARCHAR(100) NOT NULL,
-	"address" VARCHAR(100) NOT NULL,
-	zip_code CHAR(4) NOT NULL,
-	phone_no CHAR(8) NOT NULL,
-	total_paid MONEY NOT NULL,
-	PRIMARY KEY(email),
-	FOREIGN KEY(zip_code) REFERENCES TCity
+	iUser_id int IDENTITY NOT NULL,
+	cEmail VARCHAR(100) NOT NULL,
+	cFirst_name VARCHAR(100) NOT NULL,
+	cLast_name VARCHAR(100) NOT NULL,
+	cAddress VARCHAR(100) NOT NULL,
+	cZip_code CHAR(4) NOT NULL,
+	cPhone_no CHAR(8) NOT NULL,
+	fTotal_paid MONEY NOT NULL,
+	PRIMARY KEY(cEmail),
+	FOREIGN KEY(cZip_code) REFERENCES TCity
 	);
 
 
 CREATE TABLE TProduct(
-	product_id INT IDENTITY NOT NULL,
-	"name" VARCHAR(100) NOT NULL,
-	"description" TEXT NOT NULL,
-	unit_price SMALLMONEY NOT NULL,
-	average_rating numeric(3,2)
-	PRIMARY KEY(product_id)
+	iProduct_id INT IDENTITY NOT NULL,
+	cName VARCHAR(100) NOT NULL,
+	cDescription varchar(1400) NOT NULL,
+	fUnit_price SMALLMONEY NOT NULL,
+	fAverage_rating numeric(3,2)
+	PRIMARY KEY(iProduct_id)
 	);
 
 CREATE TABLE TRating(
-	product_id INT NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	rating NUMERIC(1,0) NOT NULL,
-	comment TEXT,
-	PRIMARY KEY(product_id, email),
-	FOREIGN KEY(product_id) REFERENCES TProduct,
-	FOREIGN KEY(email) REFERENCES TUser
+	iProduct_id INT NOT NULL,
+	cEmail VARCHAR(100) NOT NULL,
+	iRating NUMERIC(1,0) NOT NULL,
+	cComment VARCHAR(1400),
+	PRIMARY KEY(iProduct_id, cEmail),
+	FOREIGN KEY(iProduct_id) REFERENCES TProduct,
+	FOREIGN KEY(cEmail) REFERENCES TUser
 	);
 
 CREATE TABLE TCreditCard(
 	--check credit card length IN DENMARK
-	credit_card_no VARCHAR(16) NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	cardholder_name VARCHAR(200),
-	expiration DATE NOT NULL,
-	CCV NUMERIC(3,0) NOT NULL,
-	PRIMARY KEY(credit_card_no),
-	FOREIGN KEY(email) REFERENCES TUser
+	cCredit_card_no CHAR(16) NOT NULL,
+	cEmail VARCHAR(100) NOT NULL,
+	cCardholder_name VARCHAR(200),
+	cExpiration DATE NOT NULL,
+	cCCV NUMERIC(3,0) NOT NULL,
+	PRIMARY KEY(cCredit_card_no),
+	FOREIGN KEY(cEmail) REFERENCES TUser
 	);
 
 CREATE TABLE TInvoice(
-	invoice_id INT IDENTITY NOT NULL,
-	"date" DATE NOT NULL,
-	vat NUMERIC(2,0) NOT NULL,
-	total_price DECIMAL NOT NULL,
-	email VARCHAR(100) NOT NULL,
-	credit_card_no VARCHAR(16) NOT NULL,
-	PRIMARY KEY(invoice_id),
-	FOREIGN KEY(email) REFERENCES TUser,
-	FOREIGN KEY(credit_card_no) REFERENCES TCreditCard
+	iInvoice_id INT IDENTITY NOT NULL,
+	dDate DATETIME2 NOT NULL,
+	iVat NUMERIC(2,0) NOT NULL,
+	fTotal_price MONEY NOT NULL,
+	cEmail VARCHAR(100) NOT NULL,
+	cCredit_card_no CHAR(16) NOT NULL,
+	PRIMARY KEY(iInvoice_id),
+	FOREIGN KEY(cEmail) REFERENCES TUser,
+	FOREIGN KEY(cCredit_card_no) REFERENCES TCreditCard
 	);
 
 CREATE TABLE TInvoice_line(
-	invoice_line_id INT IDENTITY NOT NULL,
-	product_id INT NOT NULL,
-	invoice_id INT NOT NULL,
-	price MONEY NOT NULL,
-	quantity INT NOT NULL,
-	PRIMARY KEY(invoice_line_id),
-	FOREIGN KEY(product_id) REFERENCES TProduct,
-	FOREIGN KEY(invoice_id) REFERENCES TInvoice
+	iInvoice_line_id INT IDENTITY NOT NULL,
+	iProduct_id INT NOT NULL,
+	iInvoice_id INT NOT NULL,
+	fPrice SMALLMONEY NOT NULL,
+	iQuantity INT NOT NULL,
+	PRIMARY KEY(iInvoice_line_id),
+	FOREIGN KEY(iProduct_id) REFERENCES TProduct,
+	FOREIGN KEY(iInvoice_id) REFERENCES TInvoice
 	);
