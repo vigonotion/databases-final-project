5f9a6375ef1cc7b3d83758619f105b9fccbb7cf0
Tom Schneider <tom.schneider-github@sutomaji.net>
2019-12-02 12:10:43 +0100

    add restricted user laura, close #9

--- database/security/Logins_and_Users.sql
+++ database/security/Logins_and_Users.sql
@@ -13,3 +13,11 @@ ALTER ROLE [db_owner] ADD MEMBER DbAdmin
 CREATE LOGIN business01	WITH PASSWORD = '123'
 CREATE USER peter FOR LOGIN business01
 ALTER ROLE db_datareader ADD MEMBER peter
+
+
+-- A user with restricted reading privileges, which will be unable to see invoice-related information
+create login restricted01 with password = 'letmein'
+create user laura for login restricted01
+alter role db_datareader add member laura
+deny select on TInvoice to laura
+deny select on TInvoiceLine to laura
