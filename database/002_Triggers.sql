90f644aee390454e553c40fbf3bb670f3dfdfa6e
DESKTOP-1B2N9JN\caoth <caothibinhminh@gmail.com>
2019-12-16 10:12:39 +0100

    clean

--- database/002_Triggers.sql
+++ database/002_Triggers.sql
@@ -3,7 +3,7 @@ CREATE OR ALTER trigger UpdateAvgRatingForProduct ON TRating after INSERT
 AS
 BEGIN
     DECLARE @iProductId int
-    SELECT @iProductId = iProductId FROM INSERTed
+    SELECT @iProductId = iProductId FROM INSERTED
     print N'Updating avg rating ON the product:'
     print @iProductId
     EXEC SetAvgProductRating @iProductId
@@ -14,7 +14,7 @@ CREATE OR ALTER trigger UpdateProductInStock ON TInvoiceLine after INSERT
 AS
 BEGIN
     DECLARE @iProductId int
-    SELECT @iProductId = iProductId FROM INSERTed
+    SELECT @iProductId = iProductId FROM INSERTED
     print N'Updating product in stock:'
     print @iProductId
     EXEC dbo.ProductinStock @iProductId
@@ -26,7 +26,7 @@ CREATE OR ALTER trigger UpdateTotalPrice ON TInvoiceLine after INSERT
 AS
 BEGIN
     DECLARE @iInvoiceId int
-    SELECT @iInvoiceId = iInvoiceId FROM INSERTed
+    SELECT @iInvoiceId = iInvoiceId FROM INSERTED
     print N'Updating Invoice Total Price :'
     print @iInvoiceId
     EXEC dbo.TotalmONey @iInvoiceId
@@ -38,7 +38,7 @@ CREATE OR ALTER trigger UpdateTotalPaid ON TInvoice after INSERT
 AS
 BEGIN
     DECLARE @iUserId int
-    SELECT @iUserId = iUserId FROM INSERTed
+    SELECT @iUserId = iUserId FROM INSERTED
     print N'Updating user total paid:'
     print @iUserId
     EXEC dbo.Totalpaid @iUserId
