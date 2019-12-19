ee0f0ecd8eea73269e7a9f61b22f059dc2ce38ab
pokealai <poxx0003@stud.kea.dk>
2019-12-19 09:01:41 +0100

    remove index for cDescription

--- database/005_Indexes.sql
+++ database/005_Indexes.sql
@@ -1,16 +1,18 @@
 /*
 	Indexes for database: Rainforest
 */
+USE Rainforest
+GO
 
 --TRating
 CREATE INDEX IDX_iUserId ON TRating (iUserId)
 
 --TProduct
 CREATE INDEX IDX_cName ON TProduct (cName);
-CREATE INDEX IDX_cDescription ON TProduct (cDescription);
+
+--TCreditCard
+CREATE INDEX IDX_iUserId ON TCreditCard (iUserId);
 
 --TUser
 --Email index automatically created due to unique constraint
 
---TCreditCard
-CREATE INDEX IDX_iUserId ON TCreditCard (iUserId);
